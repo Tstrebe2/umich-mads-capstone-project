dd15e6fd323ff6aa20367b76825fbbc6748c995e
tstrebe2 <tstrebe2@gmail.com>
2022-11-11 18:06:23 -0500

    nothing in particular

--- tstrebe2/chest-xray-14/cx14.py
+++ tstrebe2/chest-xray-14/cx14.py
@@ -10,54 +10,19 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from PIL import Image
 
-class LRScheduler():
-    """
-    Learning rate scheduler. If the validation loss does not decrease for the 
-    given number of `patience` epochs, then the learning rate will decrease by
-    by given `factor`.
-    """
-    def __init__(self, optimizer, patience=3, min_lr=5e-5, factor=0.75):
-        """
-        new_lr = old_lr * factor
-        :param optimizer: the optimizer we are using
-        :param patience: how many epochs to wait before updating the lr
-        :param min_lr: least lr value to reduce to while updating
-        :param factor: factor by which the lr should be updated
-        """
-        self.optimizer = optimizer
-        self.patience = patience
-        self.min_lr = min_lr
-        self.factor = factor
-        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau( 
-                self.optimizer,
-                mode='min',
-                patience=self.patience,
-                factor=self.factor,
-                min_lr=self.min_lr,
-                verbose=True)
-        
-    def __call__(self, val_loss):
-        # take one step of the learning rate scheduler while providing the validation loss as the argument
-        self.lr_scheduler.step(val_loss)
-
 # define the LightningModule
 class Densenet121(pl.LightningModule):
-    def __init__(self, learning_rate=1e-3, momentum=.9, weight_decay=1e-4, input_channels=1, out_features=15):
+    def __init__(self, learning_rate=1e-3, momentum=.9, weight_decay=1e-4, class_weights=None, input_channels=1, out_features=15):
         super().__init__()
-        self.save_hyperparameters('learning_rate', 'momentum', 'weight_decay')
-        
-        self.input_channels=input_channels
-        self.out_features=out_features
+        self.automatic_optimization = True
+        self.save_hyperparameters('learning_rate', 'momentum', 'weight_decay', 'class_weights')
         
         densenet = torchvision.models.densenet121(weights='DEFAULT')
         self.features = densenet.features
         self.features.conv0 = torch.nn.Conv2d(input_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.classifier = torch.nn.Linear(in_features=densenet.classifier.in_features, 
-                                    out_features=self.out_features, 
-                                    bias=True)
-        
-        self.criterion = torch.nn.CrossEntropyLoss()
-        self.lr_scheduler = None
+                                            out_features=out_features, 
+                                            bias=True)
         
     def forward(self, x):
         features = self.features(x)
@@ -70,33 +35,33 @@ class Densenet121(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         inputs, targets = batch
         outputs = self(inputs)
-        loss = self.criterion(outputs, targets)
-        # Logging to TensorBoard by default
-        self.log("train_loss", loss)
-        return loss
+        class_weights = self.hparams.class_weights.to(self.device)
+        train_loss = torch.nn.functional.cross_entropy(outputs, targets.squeeze(), weight=class_weights)
+        self.log("train_loss", train_loss, sync_dist=True)
+        return train_loss
     
     def validation_step(self, batch, batch_idx):
-        # this is the validation loop
         inputs, targets = batch
         outputs = self(inputs)
-        val_loss = self.criterion(outputs, targets)
-        self.lr_scheduler(val_loss)
-        self.log("val_loss", val_loss)
+        class_weights = self.hparams.class_weights.to(self.device)
+        val_loss = torch.nn.functional.cross_entropy(outputs, targets.squeeze(), weight=class_weights)
+        self.log("val_loss", val_loss, sync_dist=True)
     
     def test_step(self, batch, batch_idx):
-        # this is the test loop
         inputs, targets = batch
         outputs = self(inputs)
-        test_loss = self.criterion(outputs, targets)
-        self.log("test_loss", test_loss)
+        class_weights = self.hparams.class_weights.to(self.device)
+        test_loss = torch.nn.functional.cross_entropy(outputs, targets.squeeze(), weight=class_weights)
+        self.log("test_loss", test_loss, sync_dist=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.parameters()), 
+        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.classifier.parameters()), 
                                     lr=self.hparams.learning_rate, 
                                     momentum=self.hparams.momentum, 
                                     weight_decay=self.hparams.weight_decay)
-        self.lr_scheduler = LRScheduler(optimizer)
-        return optimizer
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, min_lr=5e-5, factor=0.75, mode='min', verbose=True)
+        
+        return {"optimizer": optimizer, "lr_scheduler": {'scheduler':lr_scheduler, "monitor": "val_loss", 'interval':'epoch', 'frequency':1}}
     
 class FeaturesFreezeUnfreeze(pl.callbacks.BaseFinetuning):
     def __init__(self, unfreeze_at_epoch=10):
@@ -105,36 +70,25 @@ class FeaturesFreezeUnfreeze(pl.callbacks.BaseFinetuning):
 
     def freeze_before_training(self, pl_module):
         # freeze any module you want
-        # Here, we are freezing `feature_extractor`
-        self.freeze(pl_module.features)
+        # Here, we are freezing `features`
+        print('freezing feature weights')
+        self.freeze(pl_module.features, train_bn=False)
 
     def finetune_function(self, pl_module, current_epoch, optimizer, optimizer_idx):
-         # When `current_epoch` is 10, feature_extractor will start training.
+         # When `current_epoch` is 10, features will start training.
         if current_epoch == self._unfreeze_at_epoch:
+            print('unfreezing feature weights')
             self.unfreeze_and_add_param_group(
-                modules=pl_module.feature_extractor,
+                modules=[pl_module.features],
                 optimizer=optimizer,
-                train_bn=True)
-            
-class FineTuneBatchSizeFinder(pl.callbacks.BatchSizeFinder):
-    def __init__(self, milestones, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.milestones = milestones
-
-    def on_fit_start(self, *args, **kwargs):
-        return
-
-    def on_train_epoch_start(self, trainer, pl_module):
-        if trainer.current_epoch in self.milestones or trainer.current_epoch == 0:
-            self.scale_batch_size(trainer, pl_module)
+                train_bn=False)
     
 class CX14Dataset(Dataset):
-    def __init__(self, img_dir, img_targets, num_classes, transform=None):
+    def __init__(self, img_dir, img_targets, transform=None):
         self.img_targets = img_targets
 
         self.img_dir = img_dir
         self.transform = transform
-        self.num_classes = num_classes
 
     def __len__(self):
         return len(self.img_targets)
@@ -149,9 +103,8 @@ class CX14Dataset(Dataset):
         
         targets = np.array([self.img_targets.iloc[idx, -1]])
         targets = torch.from_numpy(targets)
-        targets = torch.nn.functional.one_hot(targets, num_classes=self.num_classes).squeeze().float()
-
+        
         return image, targets
     
-def get_data_loader(dataset, batch_size, shuffle=False, num_workers=4):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+def get_data_loader(dataset, batch_size, shuffle=False, num_workers=4, pin_memory=True):
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
--- tstrebe2/chest-xray-14/train.py
+++ tstrebe2/chest-xray-14/train.py
@@ -13,19 +13,120 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import socket
+import argparse
 
-os.environ['MASTER_ADDR'] = socket.gethostname()
+parser = argparse.ArgumentParser(
+                    prog = 'Densenet trainer.',
+                    description = 'This script trains a densenet model on the chest x-ray 14 dataset.',
+                    epilog = 'For help append train.py with --help')
 
-image_dir = '/home/tstrebel/assets/chest-xray-14/images/images'
-target_dir = '/home/tstrebel/assets/chest-xray-14'
+parser.add_argument('--batch_size', 
+                    nargs='?', 
+                    default=16, 
+                    help='Enter batch size for train & val.', 
+                    type=int, 
+                    required=False)
 
+parser.add_argument('--epochs', 
+                    nargs='?', 
+                    default=25, 
+                    help='Enter the total number of training epochs.', 
+                    type=int, 
+                    required=False)
+
+parser.add_argument('--learning_rate', 
+                    nargs='?', 
+                    default=1e-3, 
+                    help='enter the learning rate.', 
+                    type=float, 
+                    required=False)
+
+parser.add_argument('--momentum', 
+                    nargs='?', 
+                    default=.9, 
+                    help='enter momentum.', 
+                    type=float, 
+                    required=False)
+
+parser.add_argument('--weight_decay', 
+                    nargs='?', 
+                    default=1e-4, 
+                    help='enter weight decay.', 
+                    type=float, 
+                    required=False)
+
+parser.add_argument('--num_stop_rounds', 
+                    nargs='?', 
+                    default=7, 
+                    help='enter number of rounds for no improvement to trigger early stopping.', 
+                    type=int, 
+                    required=False)
+
+parser.add_argument('--num_frozen_epochs', 
+                    nargs='?', 
+                    default=10, 
+                    help='Enter the # of epochs to train while feature hidden layers are frozen.', 
+                    type=int, 
+                    required=False)
+
+parser.add_argument('--image_dir', 
+                    nargs='?', 
+                    default='assets/chest-xray-14/images/images', 
+                    help='Enter directory to training images.', 
+                    required=False)
+
+parser.add_argument('--target_dir', 
+                    nargs='?', 
+                    default='assets/chest-xray-14', 
+                    help='Enter directory to csv file with targets.', 
+                    required=False)
+
+parser.add_argument('--models_dir', 
+                    nargs='?', 
+                    default='models/cx14/', 
+                    help='Directory to save models.', 
+                    required=False)
+
+parser.add_argument('--fast_dev_run', 
+                    nargs='?', 
+                    default=0, 
+                    help='Flag to run quick train & val debug session on 1 batch.',
+                    type=int,
+                    required=False)
+
+parser.add_argument('--num_nodes', 
+                    nargs='?', 
+                    default=2, 
+                    help='Number of nodes for training.',
+                    type=int,
+                    required=False)
+
+parser.add_argument('--num_workers', 
+                    nargs='?', 
+                    default=4, 
+                    help='Number of workers from each nodeto assign to the data loader.',
+                    type=int,
+                    required=False)
+
+parser.add_argument('--restore_ckpt_path', 
+                    nargs='?', 
+                    default='models/cx14/cx14-densenet-best.ckpt', 
+                    help='Path to checkpoint to resume training.', 
+                    required=False)
+
+args = parser.parse_args()
+
+if args.restore_ckpt_path == 'None':
+    restore_ckpt_path=None
+else:
+    restore_ckpt_path = args.restore_ckpt_path
+    
 target_map = {'No Finding':0, 'Atelectasis':1, 'Cardiomegaly':2, 'Consolidation':3, 'Edema':4,
        'Effusion':5, 'Emphysema':6, 'Fibrosis':7, 'Hernia':8, 'Infiltration':9,
        'Mass':10, 'Nodule':11, 'Pleural_Thickening':12, 'Pneumonia':13, 'Pneumothorax':14}
 
-train_val_df = pd.read_csv(os.path.join(target_dir, 'train_val_list.txt'), header=None, index_col=0).index
-target_df = pd.read_csv(os.path.join(target_dir, 'Data_Entry_2017.csv'), usecols=[0, 1])
+train_val_df = pd.read_csv(os.path.join(args.target_dir, 'train_val_list.txt'), header=None, index_col=0).index
+target_df = pd.read_csv(os.path.join(args.target_dir, 'Data_Entry_2017.csv'), usecols=[0, 1])
 target_df.columns = ['file_path', 'target']
 target_df = target_df[(target_df['file_path'].isin(train_val_df)) & ~(target_df.target.str.contains('\|'))]
 target_df.target = target_df.target.map(target_map)
@@ -39,57 +140,76 @@ del(X_train)
 del(X_val)
 del(X_test)
 
+class_weights = (1-target_df.loc[train_ix].target.value_counts() / len(target_df)).sort_index().round(2).values
+class_weights = torch.from_numpy(class_weights).float()
+
 mean = [0.5341]
 std = [0.2232]
 
 train_transform = torchvision.transforms.Compose([
     torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.RandomRotation((-2, 2)),
-    torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1),
+    torchvision.transforms.RandomRotation((-4, 4)),
+    torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2),
     torchvision.transforms.Resize(512),
-    torchvision.transforms.CenterCrop(448),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(mean, std),
 ])
 
 val_transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize(512),
-    torchvision.transforms.CenterCrop(448),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(mean, std),
 ])
 
-train_dataset = cx14.CX14Dataset(image_dir, target_df.loc[train_ix], num_classes=len(target_map), transform=train_transform)
-val_dataset = cx14.CX14Dataset(image_dir, target_df.loc[val_ix], num_classes=len(target_map), transform=val_transform)
+train_dataset = cx14.CX14Dataset(args.image_dir, 
+                                 target_df.loc[train_ix], 
+                                 transform=train_transform)
+val_dataset = cx14.CX14Dataset(args.image_dir, 
+                               target_df.loc[val_ix], 
+                               transform=val_transform)
 
-train_loader = cx14.get_data_loader(train_dataset, batch_size=22, shuffle=True)
-val_loader = cx14.get_data_loader(val_dataset, batch_size=22)
+train_loader = cx14.get_data_loader(train_dataset, 
+                                    batch_size=args.batch_size, 
+                                    num_workers=args.num_workers, 
+                                    shuffle=True)
+val_loader = cx14.get_data_loader(val_dataset, 
+                                  batch_size=args.batch_size, 
+                                  num_workers=args.num_workers)
 
-model = cx14.Densenet121()
+model = cx14.Densenet121(learning_rate=args.learning_rate, 
+                         momentum=args.momentum, 
+                         weight_decay=args.weight_decay, 
+                         class_weights=class_weights)
 
-checkpoint_cb = pl.callbacks.ModelCheckpoint(dirpath='/home/tstrebel/models/',
-                                             filename='cx14-densenet.pt',
+checkpoint_cb = pl.callbacks.ModelCheckpoint(dirpath=args.models_dir,
+                                             filename='cx14-densenet',
                                              monitor='val_loss',
+                                             save_top_k=2,
+                                             save_last=True,
                                              verbose=True,
-                                             save_top_k=-1,
                                              mode='min',
                                              save_weights_only=False)
-freeze_unfreeze_cb = cx14.FeaturesFreezeUnfreeze()
+
+freeze_unfreeze_cb = cx14.FeaturesFreezeUnfreeze(unfreeze_at_epoch=args.num_frozen_epochs)
+
 early_stopping_cb = pl.callbacks.EarlyStopping(monitor='val_loss',
                                                min_delta=1e-3,
-                                               patience=5,
+                                               patience=args.num_stop_rounds,
                                                verbose=True,
                                                mode='min',
                                                check_finite=True)
-lr_monitor_cb = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
 
-logger = pl.loggers.TensorBoardLogger(save_dir=os.getcwd(), version=1, name="lightning_logs")
+lr_monitor_cb = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
 
 trainer = Trainer(accelerator='gpu', 
                   devices=-1,
-                  num_nodes=2,
+                  num_nodes=args.num_nodes,
                   callbacks=[checkpoint_cb, freeze_unfreeze_cb, early_stopping_cb, lr_monitor_cb],
-                  logger=logger,
-                  max_epochs=2)
+                  logger=True,
+                  max_epochs=args.epochs,
+                  fast_dev_run=bool(args.fast_dev_run))
 
-trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+trainer.fit(model=model, 
+            train_dataloaders=train_loader, 
+            val_dataloaders=val_loader,
+            ckpt_path=restore_ckpt_path)
